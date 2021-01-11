1507942eb944481c23cd3400032b5245d34eb7cd
vagrant <morio4899@gmail.com>
2021-01-11 03:38:39 +0000

    sample_app-last-commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+ Refile.secret_key = '3dae27b23532778accd4014099b5c48f49076ea82cded1b66e63917186b3100b7a2c8078c14eff87af81a61641922217b16bcd8e0ad8bbbc667cd78b3df77aa7'
